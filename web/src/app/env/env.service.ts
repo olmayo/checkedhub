5627945a1a912f8b6fe8458aeb996b332d127eca
Theo Godard <theo.godard@gmail.com>
2024-05-06 21:19:41 +0900

    Ommit the port number if port is 80

--- web/src/app/env/env.service.ts
+++ web/src/app/env/env.service.ts
@@ -7,7 +7,8 @@ import { env } from './env';
 export class EnvService {
 
   get backendUrl() {
-    return `${env.backend_prot}://${env.backend_host}${env.backend_port?':':''}${env.backend_port}/`;
+    let port = env.backend_port && env.backend_port!='80' ? env.backend_port : undefined;
+    return `${env.backend_prot}://${env.backend_host}${ port ? `:${port}` : '' }/`;
   }
 
 }
