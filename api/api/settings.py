deead4796b5087e67a85e94211b883b6b42e88af
Theo Godard <theo.godard@gmail.com>
2024-05-28 01:08:30 +0900

    Adding CORS app configuration

--- api/api/settings.py
+++ api/api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'polymorphic',
     'rest_framework',
     'checkedhub'
