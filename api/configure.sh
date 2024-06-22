5d3abbd847500aab48d01b2438c11db65b87095f
Theo Godard <theo.godard@gmail.com>
2024-06-22 12:13:46 +0900

    Update the path in configure script

--- api/configure.sh
+++ api/configure.sh
@@ -11,8 +11,9 @@ source "$ENV_FILE"
 set +o allexport
 
 # Copy template file to target env file
-cp api/settings_local_base.py api/settings_local.py
+cp ./api/settings_local_base.py ./api/settings_local.py
+ls ./api
 
 # Replace values in target env file
-sed -i '' -e 's|GOOGLE_OAUTH2_KEY_VALUE|'"$GOOGLE_OAUTH2_KEY_VALUE"'|g' api/settings_local.py
-sed -i '' -e 's|GOOGLE_OAUTH2_SECRET_VALUE|'"$GOOGLE_OAUTH2_SECRET_VALUE"'|g' api/settings_local.py
+sed -i '' -e 's|GOOGLE_OAUTH2_KEY_VALUE|'"$GOOGLE_OAUTH2_KEY_VALUE"'|g' ./api/settings_local.py
+sed -i '' -e 's|GOOGLE_OAUTH2_SECRET_VALUE|'"$GOOGLE_OAUTH2_SECRET_VALUE"'|g' ./api/settings_local.py
