ad38d9dd2b5a723e46076e4d6843eca8ef52b095
Theo Godard <theo.godard@gmail.com>
2024-06-23 22:52:36 +0900

    Ommit the sed command

--- api/configure.sh
+++ api/configure.sh
@@ -12,9 +12,7 @@ set +o allexport
 
 # Copy template file to target env file
 cp ./api/settings_local_base.py ./api/settings_local.py
-ls ./api
 
 # Replace values in target env file
-cat ./api/settings_local.py
-sed -i'.bak' -e 's|GOOGLE_OAUTH2_KEY_VALUE|'"$GOOGLE_OAUTH2_KEY_VALUE"'|g' ./api/settings_local.py
-sed -i'.bak' -e 's|GOOGLE_OAUTH2_SECRET_VALUE|'"$GOOGLE_OAUTH2_SECRET_VALUE"'|g' ./api/settings_local.py
+# sed -i -e 's|GOOGLE_OAUTH2_KEY_VALUE|'"$GOOGLE_OAUTH2_KEY_VALUE"'|g' ./api/settings_local.py
+# sed -i -e 's|GOOGLE_OAUTH2_SECRET_VALUE|'"$GOOGLE_OAUTH2_SECRET_VALUE"'|g' ./api/settings_local.py
