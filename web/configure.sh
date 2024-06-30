bca93be7069b0a5a6c414c2882172ab19e375f65
Theo Godard <theo.godard@gmail.com>
2024-06-30 17:23:43 +0900

    Fix the frontend sed commands

--- web/configure.sh
+++ web/configure.sh
@@ -25,7 +25,7 @@ set +o allexport
 cp src/app/env/env-base.ts src/app/env/env.ts
 
 # Replace values in target env file
-sed -i '' -e 's|BACKEND_PROT_PLACEHOLDER|'"$BACKEND_PROT"'|g' src/app/env/env.ts
-sed -i '' -e 's|BACKEND_HOST_PLACEHOLDER|'"$BACKEND_HOST"'|g' src/app/env/env.ts
-sed -i '' -e 's|BACKEND_PORT_PLACEHOLDER|'"$BACKEND_PORT"'|g' src/app/env/env.ts
-sed -i '' -e 's|GOOGLE_OAUTH2_SECRET_VALUE|'"$GOOGLE_OAUTH2_SECRET_VALUE"'|g' src/app/env/env.ts
+sed -i -e 's|BACKEND_PROT_PLACEHOLDER|'"$BACKEND_PROT"'|g' src/app/env/env.ts
+sed -i -e 's|BACKEND_HOST_PLACEHOLDER|'"$BACKEND_HOST"'|g' src/app/env/env.ts
+sed -i -e 's|BACKEND_PORT_PLACEHOLDER|'"$BACKEND_PORT"'|g' src/app/env/env.ts
+sed -i -e 's|GOOGLE_OAUTH2_SECRET_VALUE|'"$GOOGLE_OAUTH2_SECRET_VALUE"'|g' src/app/env/env.ts
